dfd69e81f2297ec4861c638744d05ba69391506c
1177050057 <karimaa.ms29@gmail.com>
2021-01-20 14:24:02 +0700

    Unity 2017

---
+++ Assets/Script/Play.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Play : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void scale (float scale)
+	{
+		transform.localScale = new Vector2 (1/scale,1*scale);
+	}
+	public void scene(string scene)
+	{
+		Application.LoadLevel(scene);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
